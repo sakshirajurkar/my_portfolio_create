8c797192953337c0bf931c12b1c709408cc14849
sakshi <sakshirajurkar09@gmail.com>
2023-07-16 22:07:34 -0800

    update added

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let menu = document.querySelector('#menu-btn');
-let header = document.querySelector('.header');
+let menu = document.querySelector('#menu-bars');
+let header = document.querySelector('header');
 
 menu.onclick = () =>{
     menu.classList.toggle('fa-times');
@@ -11,13 +11,26 @@ window.onscroll = () =>{
     header.classList.remove('active');
 }
 
-let themeToggler = document.querySelector('#theme-toggler');
+let cursor1 = document.querySelector('.cursor-1');
+let cursor2 = document.querySelector('.cursor-2');
 
-themeToggler.onclick = () =>{
-    themeToggler.classList.toggle('fa-sun');
-    if(themeToggler.classList.contains('fa-sun')){
-        document.body.classList.add('active');
-    }else{
-        document.body.classList.remove('active');
-    }
+window.onmousemove = (e) =>{
+    cursor1.style.top = e.pageY + 'px';
+    cursor1.style.left = e.pageX + 'px';
+    cursor2.style.top = e.pageY + 'px';
+    cursor2.style.left = e.pageX + 'px';
 }
+
+document.querySelectorAll('a').forEach(links =>{
+
+    links.onmouseenter = () =>{
+        cursor1.classList.add('active');
+        cursor2.classList.add('active');
+    }
+
+    links.onmouseleave = () =>{
+        cursor1.classList.remove('active');
+        cursor2.classList.remove('active');
+    }
+
+});
